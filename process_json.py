21ef4e9a85e183ed51eaf3a7ee61108a85295f5c
Clare Maier <clare.m200@gmail.com>
2017-07-22 16:30:43 +0000

    second attempt at github

---
+++ process_json.py
@@ -0,0 +1,12 @@
+#inspired by https://stackoverflow.com/questions/4746190/find-and-replace-within-a-text-file-using-python
+
+n = 1
+f1=open("search.json", "r")
+f2=open("output.json", "w")
+for line in f1:
+  line = '"tweet' + str(n) + '": ' + line + ','
+  #line = line.replace('"tweet"', '"tweet' + str(n) + '"')
+  n = n+1
+  f2.write(line)
+f1.close()
+f2.close()
